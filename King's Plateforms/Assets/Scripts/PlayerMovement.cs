5d94938ad8f933213aaf4dc0f8ac8d9ec17b0a46
HexaFred <fredbat95@gmail.com>
2024-12-04 13:21:33 +0100

    Ajout du système de sauts
    Sera amélioré

--- King's Plateforms/Assets/Scripts/PlayerMovement.cs
+++ King's Plateforms/Assets/Scripts/PlayerMovement.cs
@@ -3,14 +3,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
+    public float jumpForce;
+
+    private bool isJumping;
+    private bool isGrounded;
+
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
 
     public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
 
     void FixedUpdate()
     {
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+
         float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
 
+        if(Input.GetButtonDown("Jump") && isGrounded==true)
+        {
+            isJumping = true;
+        }
 
         MovePlayer(horizontalMovement);
     }
@@ -19,5 +32,11 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 targetVeloctity = new Vector2(_horizontalMovement, rb.linearVelocity.y);
         rb.linearVelocity = Vector3.SmoothDamp(rb.linearVelocity, targetVeloctity, ref velocity, .05f);
+
+        if(isJumping == true )
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping=false;
+        }
     }
 }
