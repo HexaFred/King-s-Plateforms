47632ba5c7ef811c140783705532a17f48549d07
HexaFred <fredbat95@gmail.com>
2024-12-10 14:41:52 +0100

    Add Ennemy

---
+++ King's Plateforms/Assets/Scripts/EnnemyPatrol.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class EnnemyPatrol : MonoBehaviour
+{
+    public float speed;
+    public Transform[] waypoints;
+
+    public SpriteRenderer graphics;
+    private Transform target;
+    private int destPoint = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        target = waypoints[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(speed * Time.deltaTime * dir.normalized, Space.World);
+
+        // 0.3f -> Ennemi presque arrivé au point
+        if (Vector3.Distance(transform.position, target.position) < 0.3f)
+        {
+            destPoint = (destPoint + 1) % waypoints.Length; //Si l'ennemi arrive au dernier point, on repart au premier
+            target = waypoints[destPoint];
+            graphics.flipX = !graphics.flipX;
+        }
+    }
+}
