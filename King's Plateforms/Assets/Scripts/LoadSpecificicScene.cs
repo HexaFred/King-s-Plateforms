877a3bc5d157d1c3c827d63f648b524e9e542aeb
HexaFred <fredbat95@gmail.com>
2024-12-18 15:19:51 +0100

    Add FadeSystem

--- King's Plateforms/Assets/Scripts/LoadSpecificicScene.cs
+++ King's Plateforms/Assets/Scripts/LoadSpecificicScene.cs
@@ -1,14 +1,29 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class LoadSpecificicScene : MonoBehaviour
 {
     public string sceneName;
+    public Animator fadeSystem;
+
+    private void Awake()
+    {
+        fadeSystem = GameObject.FindGameObjectWithTag("FadeSystem").GetComponent<Animator>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene(sceneName);
+            StartCoroutine(loadNextScene());
+            
         }
+    }
+
+    public IEnumerator loadNextScene()
+    {
+        fadeSystem.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneName);
     }
 }
--- King's Plateforms/Assets/Scripts/PlayerSpawn.cs
+++ King's Plateforms/Assets/Scripts/PlayerSpawn.cs
@@ -5,6 +5,7 @@ public class PlayerSpawn : MonoBehaviour
     private void Awake()
     {
         GameObject.FindGameObjectWithTag("Player").transform.position = transform.position;
+        GameObject.FindGameObjectWithTag("MainCamera").transform.position = transform.position;
     }
 
 }
